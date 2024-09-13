f97cb810f8dca2ec50c69f4c84b7e4eff041a2c9
jomout <moutasjo@gmail.com>
2024-09-13 19:30:57 +0300

    [MODIFIED]: Modified sidebar to contain icons

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,5 +7,4 @@ export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'form', component: TestFormComponent },
 ];
