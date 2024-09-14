3c02f29732555e67e7a36c78f4dd2bf752959ac6
jomout <moutasjo@gmail.com>
2024-09-14 13:29:11 +0300

    [MODIFIED]: Sidebar design implemented

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideNgIconLoader } from '@ng-icons/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideHttpClient(), 
+    // provideNgIconLoader(name => {
+    //   const http = inject(HttpClient);
+    //   return http.get(`/assets/icons/${name}.svg`, { responseType: 'text' });
+    // }),
+  ]
 };
