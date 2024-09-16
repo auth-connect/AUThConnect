21b79ccf1e14dec050cd43e595d6e20f4ed2d914
jomout <moutasjo@gmail.com>
2024-09-16 17:53:52 +0300

    [ADDED|MODIFIED]: Added sidebar fields

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { TestFormComponent } from './components/test-form/test-form.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { ThreadsComponent } from './components/threads/threads.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'threads', component: ThreadsComponent },
+  { path: 'courses', component: CoursesComponent },
   { path: 'about', component: AboutComponent },
   { path: 'form', component: TestFormComponent }
 ];
