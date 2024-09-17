1b492a9d8ffe50ba09d79b56ffbff1d2de564a2d
jomout <moutasjo@gmail.com>
2024-09-17 16:21:16 +0300

    [ADDED|MODIFIED]: Authentication service implemented

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,14 +2,15 @@ import { ApplicationConfig, inject, provideZoneChangeDetection } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideNgIconLoader } from '@ng-icons/core';
+import { authInterceptor } from './services/auth-service/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
-    provideHttpClient(), 
+    provideHttpClient(withInterceptors([authInterceptor])),
     // provideNgIconLoader(name => {
     //   const http = inject(HttpClient);
     //   return http.get(`/assets/icons/${name}.svg`, { responseType: 'text' });
--- frontend/src/app/components/auth/login-page/login-form/login-form.component.ts
+++ frontend/src/app/components/auth/login-page/login-form/login-form.component.ts
@@ -1,11 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { NgIconComponent, provideIcons, provideNgIconsConfig } from '@ng-icons/core';
 import { lucideLoader } from '@ng-icons/lucide';
+import { AuthService } from '../../../../services/auth-service/auth.service';
+import { UserInterface } from '../../../../interfaces/user.interface';
+import { AuthFacade } from '../../../../facades/auth-facade/auth-facade.service';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [NgIconComponent],
+  imports: [NgIconComponent, ReactiveFormsModule],
   viewProviders: [
     provideIcons({ lucideLoader }), 
     provideNgIconsConfig({
@@ -16,12 +26,42 @@ import { lucideLoader } from '@ng-icons/lucide';
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
+  fb = inject(FormBuilder);
+
+  authService = inject(AuthService);
+  authFacade = inject(AuthFacade);
+
+
   isLoading = signal(false);
 
-	send() {
-		this.isLoading.set(true);
+  form!: FormGroup;
+
+
+  constructor() {}
+  
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.form = this.fb.nonNullable.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+
+
+  onSubmit(): void {
+    if (this.form.invalid)
+      return;
+    this.isLoading.set(true);
 		setTimeout(() => this.isLoading.set(false), 3000);
-	}
+    const user: UserInterface = this.form.getRawValue()
+
+    console.log(user);
+    this.authFacade.loginUser(user);
+  }
 
 }
