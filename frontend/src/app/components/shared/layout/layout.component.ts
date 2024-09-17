d5e30b86c62d6218cf8788c99864d883a2054419
jomout <moutasjo@gmail.com>
2024-09-17 11:50:54 +0300

    [MODIFIED]: Basic frontend structure modified. Added lazy loading to pages

---
+++ frontend/src/app/components/shared/layout/layout.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, HostListener, input, OnInit, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../header/header.component";
+import { SidebarComponent } from "../sidebar/sidebar.component";
+
+@Component({
+  selector: 'app-main-page',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, SidebarComponent],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent implements OnInit{
+
+  isSidebarCollapsed = signal<boolean>(false);
+  screenWidth = signal<number>(window.innerWidth);
+
+  @HostListener('window:resize')
+  onResize() {
+    this.screenWidth.set(window.innerWidth);
+    if (this.screenWidth() < 768) {
+      this.isSidebarCollapsed.set(true);
+    }
+  }
+
+  ngOnInit(): void {
+    this.isSidebarCollapsed.set(this.screenWidth() < 768);
+  }
+
+  changeIsSidebarCollapsed(isSidebarCollapsed: boolean): void {
+    this.isSidebarCollapsed.set(isSidebarCollapsed);
+  }
+
+  
+  sizeClass = computed(() => {
+    const isLeftSidebarCollapsed = this.isSidebarCollapsed();
+    if (isLeftSidebarCollapsed) {
+      return '';
+    }
+    return this.screenWidth() > 768 ? 'body-trimmed' : 'body-md-screen';
+  });
+}
