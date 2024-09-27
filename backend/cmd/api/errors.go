2688ed0172eb5ca78bf8af94de4463536c7320be
ntentasd <dimitrisntentas2002@gmail.com>
2024-09-27 17:50:40 +0300

    [ADDED]: Custom errors & error functions

---
+++ backend/cmd/api/errors.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.Print(err)
+}
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, message any, status int) {
+	err := app.jsonWrite(w, message, status, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	message := "the server encountered a problem and could not process your request"
+	app.errorResponse(w, r, envelope{"error": message}, http.StatusInternalServerError)
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the request resource could not be found"
+	app.errorResponse(w, r, envelope{"error": message}, http.StatusNotFound)
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, envelope{"error": err.Error()}, http.StatusBadRequest)
+}
