580d0c20112c2987a674e07b786b1478c1d6ac9f
ntentasd <dimitrisntentas2002@gmail.com>
2024-09-27 17:50:12 +0300

    [MODIFIED]: Added some detail to the postgres fields

--- backend/migrations/000001_create_users_table.up.sql
+++ backend/migrations/000001_create_users_table.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS users (
-  id bigserial PRIMARY KEY,
-  username varchar UNIQUE NOT NULL,
-  hashed_password varchar NOT NULL,
-  full_name varchar NOT NULL,
-  role varchar NOT NULL,
-  email varchar UNIQUE NOT NULL,
-  created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+  id BIGSERIAL PRIMARY KEY,
+  user_name VARCHAR UNIQUE NOT NULL,
+  hashed_password CHAR(60) NOT NULL,
+  full_name VARCHAR NOT NULL,
+  role VARCHAR(30) NOT NULL DEFAULT 'Member',
+  email VARCHAR UNIQUE NOT NULL,
+  created_at TIMESTAMP(0) with time zone NOT NULL DEFAULT NOW()
 );
 
-ALTER TABLE users ADD CONSTRAINT unique_username_email UNIQUE (username, email);
+ALTER TABLE users ADD CONSTRAINT unique_username_email UNIQUE (user_name, email);
